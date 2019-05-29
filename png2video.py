4cd1d0f2ef45df8570f9f14f59e50a45b5a17750
Richard Shaw <49898292+xiaospica@users.noreply.github.com>
2019-05-29 14:18:58 +0800

    Update png2video.py

--- png2video.py
+++ png2video.py
@@ -1,17 +1,24 @@
 import cv2
 import os
-import tqdm
+from tqdm import tqdm
+import numpy as np
 
-path = 'D:\\CNNtest\\AliSR\\srgan-1.2.0\\samples\\evaluate\\'
+path = 'C:\\Users\\richard.xiao\\PycharmProjects\\AliSR\\srgan-1.2.0\\srgan-1.2.0\\samples\\evaluate\\'
 filelist = os.listdir(path)
+
+#filehead is 'valid_gen'
+filehead = filelist[1].split('_')[0]+'_'+filelist[1].split('_')[1]
+file_num = np.array(filelist).shape[0]
+
 out = cv2.VideoWriter('Youku_00239_l.avi', cv2.VideoWriter_fourcc('D', 'I', 'V', 'X'), 24, (1920,1080))
-for item in filelist:
-    if item.endswith('.png'):
-        # 找到路径中所有后缀名为.png的文件，可以更换为.jpg或其它
-        item = path + item
-        img = cv2.imread(item)
-        out.write(img)
+
+for item in tqdm(range(file_num)):
+
+    # print(item)
+    item_path = path + filehead + '_' + str(item) + '.png'
+    img = cv2.imread(item_path)
+    out.write(img)
+    print(item_path)
 
 out.release()
 cv2.destroyAllWindows()
-

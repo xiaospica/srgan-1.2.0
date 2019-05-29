09a726937d5f5dca16bbe48c833bf6febccdc87c
Richard Shaw <49898292+xiaospica@users.noreply.github.com>
2019-05-29 17:30:51 +0800

    Update png2y4m.py

--- png2y4m.py
+++ png2y4m.py
@@ -1,61 +1,44 @@
-import glob
 from os import path
 import os
 import subprocess
+import numpy as np
 
-# setting
-IF_PAIR = False
-DATA_PATH = "D:\\CNNtest\\AliSR\\SRdata\\youku_00200_00249_l\\"
-# DATA_PATH = "./data/round1/valid/"
+DATA_PATH = "D:\\YoukuVideoTest\\"
+test_dirs = []
 
-def y4mtopng(if_pair,dir):
-    """convert y4m video to frame image
-    if_pair: without(test data) groud truth hr video, please set IF_PAIR to False
-    dir: data directory
-    """
-    print("Working path: ",DATA_PATH)
-    lr_y4m = glob.glob(DATA_PATH + "Youku_*_l.y4m")
-    print("Found {} lr y4m video".format(len(lr_y4m)))
-    for item in lr_y4m:
+def png2y4m():
 
-        # create folder
-        dir = item.split('_')[1]
-        dir_path = path.join(DATA_PATH,dir)
-        if not path.exists(dir_path):
-            os.mkdir(dir_path)
+    print("Working path: ", DATA_PATH)
+    lr_y4m = os.walk(DATA_PATH)
 
-        # create lr folder for png file
-        lr_dir_path = path.join(dir_path,'lr')
-        if not path.exists(lr_dir_path):
-            os.mkdir(lr_dir_path)
+    # find test video directory
+    for root,dirs,filename in lr_y4m:
+        print('root:',root)
+        if dirs:
+            test_dirs = dirs
+            print("Found {} video directories".format(np.array(dirs).shape[0]))
+            print('dirs:',dirs)
+            break
 
-        # process lr video
-        png_reg = lr_dir_path + "/%3d.png"
+    # creat folder for video
+    dir_path = path.join(DATA_PATH,'submit_videos')
+    if not path.exists(dir_path):
+        os.mkdir(dir_path)
+
+    # convert png to y4m
+    for item in test_dirs:
+
+        # process SR video
+        SR_video_path = path.join(dir_path, 'Youku_'+ item +'_l' )
+        SR_pic_path = path.join(DATA_PATH,item)
+
+        video_path = SR_video_path + '.y4m'
+        SR_pic = SR_pic_path + "/%3d.png"
+        print('SR_pic:', SR_pic)
         print("Processing {} ...".format(item))
-        cmd_str = "ffmpeg -i " + item + " -vsync 0 " + png_reg + " -y"
+        cmd_str = 'ffmpeg -i ' + SR_pic + ' -pix_fmt yuv420p -vsync 0 ' + video_path + ' -y'
         p = subprocess.Popen(cmd_str, shell=True)
         p.wait()
-
-        if if_pair:
-
-            # create hr folder for png file
-            hr_dir_path = path.join(dir_path,'hr')
-            if not path.exists(hr_dir_path):
-                os.mkdir(hr_dir_path)
-
-            # process lr video
-            tmp = item.split('_')
-            tmp[2] = "h_GT.y4m"
-            hr_item = ('_').join(tmp)
-            png_reg = hr_dir_path + "/%3d.png"
-            print("Processing {} ...".format(hr_item))
-            cmd_str = "ffmpeg -i " + hr_item + " -vsync 0 " + png_reg + " -y"
-            p = subprocess.Popen(cmd_str, shell=True)
-            p.wait()
-
-
+#
 if __name__ == "__main__":
-    y4mtopng(IF_PAIR,DATA_PATH)
-
-
-
+    png2y4m()
